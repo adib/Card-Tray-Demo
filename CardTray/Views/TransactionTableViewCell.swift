0436e015337821262806700d16c225837bec4a8f
Sasmito Adibowo <adib@basil-salad.com>
2016-06-14 13:13:28 +0800

    Card Transaction List

---
+++ CardTray/Views/TransactionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionTableViewCell.swift
+//  CardTrayDemo
+//
+//  Created by Sasmito Adibowo on 14/6/16.
+//  Copyright © 2016 Basil Salad Software. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
